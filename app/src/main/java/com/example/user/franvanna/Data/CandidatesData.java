191f20d93a6ce36931831c3ff0c4deebe3948a28
Docta Rhyf <drrhyf@gmail.com>
2018-04-10 00:07:47 +0200

    Print result design

---
+++ app/src/main/java/com/example/user/franvanna/Data/CandidatesData.java
@@ -0,0 +1,169 @@
+package com.example.user.franvanna.Data;
+
+import android.content.Context;
+
+import com.example.user.franvanna.Objects.Candidate;
+import com.example.user.franvanna.Utils.Utils;
+import com.example.user.franvanna.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class CandidatesData {
+    public static final int CAND_TYPE_PREZ = 0;
+    private static final String TAG = "CENI";
+    private static final int MAX_CAND_PIX_NUM = 15;
+
+    public static  int getPartiLogoByName(String partiName){
+
+        //int partiLogoId = -1;
+
+        HashMap<String, Integer> partiLogos = new HashMap<>();
+
+        partiLogos.put("eps", R.drawable.pt_eps);
+        partiLogos.put("independant", R.drawable.pt_independant);
+        partiLogos.put("kozi", R.drawable.pt_kozi);
+        partiLogos.put("lion", R.drawable.pt_lion);
+        partiLogos.put("loboko", R.drawable.pt_loboko);
+        partiLogos.put("losambo", R.drawable.pt_losambo);
+        partiLogos.put("masomo", R.drawable.pt_masomo);
+        partiLogos.put("mosala", R.drawable.pt_mosala);
+        partiLogos.put("ndule", R.drawable.pt_ndule);
+        partiLogos.put("vision", R.drawable.pt_vision);
+        partiLogos.put("xdb", R.drawable.pt_xdb);
+
+        return partiLogos.get(partiName);
+
+    }
+
+    public static List<Candidate> getCandidates(Context context, int candType) {
+
+
+
+        List<Candidate> candidates = new ArrayList<>();
+
+        Scanner scanner;
+
+        int j = 0;
+        switch (candType){
+
+            case CAND_TYPE_PREZ:
+
+                /*
+                for(int i = 0; i < 10; i++) {
+
+                    candidates.add(new Candidate(i, "NOM POSTON " + i, "Prenom " + i, -1, R.drawable.pt_xdb, candType));
+
+                }*/
+
+                scanner = new Scanner(context.getResources().openRawResource(R.raw.candz_prez));
+               j = 0;
+                while (scanner.hasNextLine()){
+                   // Log.e(TAG, "line -> " + scanner.nextLine() );
+                    String line = scanner.nextLine();
+
+
+                    if(!line.isEmpty()){
+                        String[] candData = line.split(",");
+                        String fullName = candData[0];
+                        String prenom = candData[1];
+                        String partiName = candData[2];
+                        int picId = Utils.GRIDBN("cd" + (j+1), R.drawable.class);
+                        Candidate candidate = new Candidate(j,fullName, prenom,picId,getPartiLogoByName(candData[2]), candType);
+                        candidate.setPartiName(partiName);
+                        candidates.add(candidate);
+
+                    }
+                    j++;
+                }
+
+                /*
+                ango mongbwalu,jean,indépendant
+                yakusu isangi,león,eps
+                bonga fataki,justin,kozi
+                djuma inongo,freddy,lion
+                feshi mutunda,franvale,loboko
+                goma kasindi,plamedie,losambo
+                kansenya kiambi,yves,masomo
+                kongolo panda,antoine de padoue,mosala
+                mulongo songa,gaspard,ndule
+                nyakunde lolwa,deoel,vision
+                oshwe koshibanda,fabrice,xdb*/
+
+
+
+                break;
+
+            case Candidate.CAND_TYPE_LEG_NAT:
+
+                /*
+                for(int i = 0; i < 15; i++) {
+
+                    candidates.add(new Candidate(i, "NOM POSTON " + i, "Prenom " + i, -1, -1, candType));
+
+                }*/
+
+                scanner = new Scanner(context.getResources().openRawResource(R.raw.candz_dep_nat));
+                j = 0;
+                int picIdx = 1;
+                while (scanner.hasNextLine()){
+                    // Log.e(TAG, "line -> " + scanner.nextLine() );
+                    if(picIdx > MAX_CAND_PIX_NUM ) picIdx = 1;
+                    String line = scanner.nextLine();
+
+
+                    if(!line.isEmpty()){
+                        String[] candData = line.split(",");
+                        int picId = Utils.GRIDBN("cd" + picIdx, R.drawable.class);
+                        Candidate candidate = new Candidate(j,candData[0], candData[1],picId,getPartiLogoByName(candData[2]), candType);
+                        candidate.setPartiName(candData[2]);
+                        candidates.add(candidate);
+
+                    }
+                    j++;
+                    picIdx++;
+                }
+
+                break;
+
+            case Candidate.CAND_TYPE_LEG_PROV:
+
+                scanner = new Scanner(context.getResources().openRawResource(R.raw.candz_dep_prov));
+                j = 0;
+                picIdx = 1;
+                while (scanner.hasNextLine()){
+                    // Log.e(TAG, "line -> " + scanner.nextLine() );
+
+                    if(picIdx > MAX_CAND_PIX_NUM ) picIdx = 1;
+                    String line = scanner.nextLine();
+
+
+                    if(!line.isEmpty()){
+                        String[] candData = line.split(",");
+                        int picId = Utils.GRIDBN("cd" + picIdx, R.drawable.class);
+                        Candidate candidate = new Candidate(j,candData[0], candData[1],picId,getPartiLogoByName(candData[2]), candType);
+                        candidate.setPartiName(candData[2]);
+                        candidates.add(candidate);
+
+                    }
+                    j++;
+                    picIdx++;
+                }
+
+
+                break;
+        }
+
+        return candidates;
+
+    }
+
+    public static Candidate getWinningCandidate(Context context, int candType, int candId){
+
+        List<Candidate> candidates = getCandidates(context, candType);
+
+        return candidates.get(candId);
+    }
+}
---
+++ app/src/main/java/com/example/user/franvanna/Utils/Utils.java
@@ -0,0 +1,99 @@
+package com.example.user.franvanna.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Utils {
+
+    private static final String NO_VALUE = "NO_VALUE";
+    public static final String CENI_PREF_NAME = "CENI_PREF";
+    public static final String VOTE_KEY_CAND_PREZ = "candPrez";
+    private static final int NO_CANDIDATE = 0xbeef;
+    public static final String VOTE_KEY_CAND_DEP_NAT = "candDepNat";
+    public static final String VOTE_KEY_CAND_DEP_PROV = "candDepProv";
+
+    public static String UCFirst(String string){
+
+
+        return string.substring(0,1).toUpperCase()+string.substring(1);
+
+    }
+
+    // TODO: 07/04/2018 Utils.getResIdByName("pt_kozi", R.drawable.class);
+
+    public static int getResIdByName(String resName, Class<?> c){
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        }catch (Exception e){
+            throw new RuntimeException("No resource ID found for : "
+            + resName + " / " + c, e);
+        }
+    }
+
+    public static int GRIDBN(String resName, Class<?> c){
+        return getResIdByName(resName, c);
+    }
+
+    public static int RINT(int MAX_EXCLUSIF, int MIN_INCLUSIF){
+        Random r = new Random();
+        return r.nextInt(MAX_EXCLUSIF - MIN_INCLUSIF) + MIN_INCLUSIF;
+    }
+
+    public static ArrayList<View> getViewsByTag(ViewGroup root,String tag ){
+        ArrayList<View> views = new ArrayList<>();
+        final int childCount = root.getChildCount();
+
+        for(int i = 0; i < childCount; i++){
+            final View child = root.getChildAt(i);
+
+            if(child instanceof  ViewGroup){
+                views.addAll(getViewsByTag((ViewGroup) child, tag));
+            }
+
+            final Object tagObj = child.getTag();
+            if(tagObj != null && tagObj.equals(tag)){
+                views.add(child);
+            }
+        }
+
+        return views;
+    }
+
+    public static void saveCandToPref(Context context, String key, int val){
+
+        SharedPreferences preference = context.getSharedPreferences(CENI_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preference.edit();
+
+        editor.putInt(key, val);
+        editor.commit();
+
+    }
+
+    public static int getCandFromPref(Context context, String key ){
+
+        SharedPreferences preference = context.getSharedPreferences(CENI_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preference.edit();
+
+        return preference.getInt(key, NO_CANDIDATE);
+
+    }
+
+    public static boolean clearCandidatesData(Context context){
+
+
+        SharedPreferences preference = context.getSharedPreferences(CENI_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preference.edit();
+
+        editor.clear();
+
+        return  preference.getAll().size() == 0;
+    }
+}
